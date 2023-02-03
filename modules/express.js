beeee135ac028df2dcb25d0098cde8b9b8ab5b92
Yure Silva <yuresilvaesilva07@gmail.com>
2023-02-02 23:42:12 -0300

    V.4 Aprimorando interface

--- modules/express.js
+++ modules/express.js
@@ -18,9 +18,14 @@ app.get("/", async (req, res) =>{
         if(req.query.busca == null){
             const posts = await Posts.find({})
             const notice = (posts.reverse())[0]
-            res.render("home", {notice, posts})
+            const mostViews = await Posts.find({}).sort({"views": -1}).limit(4)
+            res.render("home", {notice, posts, mostViews})
         } else {
-            res.send(`Você buscou: ${req.query.busca}`) // Link para acessar: http://localhost:8080/?busca=jornal
+            Posts.find({title: {$regex: req.query.busca, $options: "i"}},(error, result) => {
+                if(error) res.send(error.message)
+                const contentBusca = req.query.busca
+                res.render("busca", {result, contentBusca}) // Link para acessar: http://localhost:8080/?busca=jornal
+            })
         }
     } catch(error){
         res.send(error.message)
@@ -31,11 +36,11 @@ app.get("/", async (req, res) =>{
 app.get("/:slug", async (req, res) =>{
     const slug = req.params.slug
     const posts = await Posts.find({})
+    const mostViews = await Posts.find({}).sort({"views": -1}).limit(4)
     const notice = posts.find(item => item.slug == slug)
-    console.log(notice)
     Posts.findOneAndUpdate({slug: slug}, {$inc: {views: 1}}, {new: true}, (error) => {
         if(error) res.send(error.message)
-        res.render("single", {posts, notice})
+        res.render("single", {posts, notice, mostViews})
     })
 })
 
